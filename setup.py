a9c07c0e12c2a32b97c010a540eb122caa128f29
weiyang <weiyang@weiyang-mac.local>
2019-05-26 00:23:32 +0800

    Use SCM version
    Signed-off-by: weiyang <weiyang@weiyang-mac.local>

--- setup.py
+++ setup.py
@@ -1,26 +1,26 @@
 import io
 import os
-import re
 
 from setuptools import find_packages, setup
 
 
 def read(filename):
     filename = os.path.join(os.path.dirname(__file__), filename)
-    text_type = type(u"")
     with io.open(filename, mode="r", encoding="utf-8") as fd:
-        return re.sub(text_type(r":[a-z]+:`~?(.*?)`"), text_type(r"``\1``"), fd.read())
+        return fd.read()
 
 
 setup(
     name="oai-request",
-    version="0.0.1",
     url="https://github.com/wy-z/oai-request",
     license="MIT",
     author="weiyang",
     author_email="weiyang.ones@gmail.com",
     description="OAIRequest is a python client library for OpenAPI 3.0",
     long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
     packages=find_packages(exclude=("tests",)),
     install_requires=["requests", "pyyaml"],
     classifiers=[
