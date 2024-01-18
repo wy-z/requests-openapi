491766103d58a048efbeb594f6807822e13f5665
weiyang <weiyang.ones@gmail.com>
2024-01-18 19:03:24 +0800

    Export load_spec_from_file/load_spec_from_url
    Signed-off-by: weiyang <weiyang.ones@gmail.com>

--- requests_openapi/__init__.py
+++ requests_openapi/__init__.py
@@ -1,4 +1,4 @@
-from .core import Client, Operation, Server, openapi
+from .core import Client, Operation, Server, openapi, load_spec_from_file, load_spec_from_url
 from .requestor import Requestor
 
-__all__ = ["openapi", "Client", "Server", "Operation", "Requestor"]
+__all__ = ["openapi", "Client", "Server", "Operation", "Requestor", "load_spec_from_file", "load_spec_from_url"]
